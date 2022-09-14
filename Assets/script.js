49e0ae8e84677710f35d43d2312d3a35c58d596b
Ultrapyre <sidneymakhiutao@yahoo.com.hk>
2022-09-15 00:56:05 +0800

    Added local storage functionality. Now to wait several hours for a good picture...

--- Assets/script.js
+++ Assets/script.js
@@ -12,6 +12,8 @@ dateEl.text(dt.toLocaleString({weekday:'long', month: 'long', day: 'numeric'}));
 /*Now to actually render the schedule itself. 
 A working day is 9am to 5pm, so first an array is made to generate the timeslots.
 An extra entry is included in the array for calculation purposes.
+
+The array below generates a series of values, which is just that day, except the hour is set to specific times.
 */
 
 var timeSlots = [
@@ -27,17 +29,30 @@ var timeSlots = [
     DateTime.fromObject({hour: 18})
 ];
 
+//It's possible for entries to have already been added before, so the upcoming timeblocks need values.
+//By default, they should be blank. So first to add a data block that should be empty.
+var scheduledEvents = ['', '', '', '', '', '', '', '', '']
+//Then to check for any possible entries already saved,
+var savedata = JSON.parse(localStorage.getItem("events"))
+if(savedata){
+    scheduledEvents = savedata;
+}
 //Now to actually create the timeblocks. Each block consists of the time itself, the input field, and the save button.
+//Since 6 pm is when (most) folks clock out, it isn't included in the schedule.
+//So the loop stops before it.
 for (var i=0;i<timeSlots.length-1;i++){
     var timeBlock = $('<div class="row time-block">');
     
     var hour = $('<div class="hour">');
     hour.css('width', '7.5%');
+    //Probably unnecessarily complicated compared to simply typing a number and 'am/pm' afterwards,
+    //but this bit here converts the times in the array into those simple-to-read values.
     hour.text(timeSlots[i].toLocaleString(DateTime.TIME_SIMPLE));
     timeBlock.append(hour);
 
     var taskInput = $('<input class="textarea">');
     taskInput.css('width', '85%');
+    taskInput.val(scheduledEvents[i]);
     //Changes the input color depending on the current time.
     if (timeSlots[i]< DateTime.now() && DateTime.now() < timeSlots[i+1]){
         taskInput.addClass('present');
@@ -48,7 +63,7 @@ for (var i=0;i<timeSlots.length-1;i++){
     else if(DateTime.now() > timeSlots[i]){
         taskInput.addClass('past');
     }
-
+    taskInput.css('color', 'black');
     timeBlock.append(taskInput);
 
     var saveBtn = $('<button class="saveBtn">');
@@ -57,3 +72,16 @@ for (var i=0;i<timeSlots.length-1;i++){
     timeBlock.append(saveBtn);
     scheduleEl.append(timeBlock);
 }
+//Those save buttons need to do something. So time to add an event listener to the schedule.
+scheduleEl.on('click', '.saveBtn', function(event){
+    
+    //Technically pressing any of those save buttons do the same thing of updating the array of events, so might as well make them all do the same thing.
+    //This loop will check for any values in the inputs, then save it to the event array.
+    for(i=0; i<$('input').length;i++){
+        if($('input').eq(i)){
+            scheduledEvents[i]=$('input').eq(i).val();
+        }
+    }
+    //Once the array's been updated, save it within local storage.
+    localStorage.setItem("events", JSON.stringify(scheduledEvents));
+})
